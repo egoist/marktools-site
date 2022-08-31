85c75363e68590f2ba5061ce7f17c365f5edaa0c
egoist <0x142857@gmail.com>
2022-08-31 16:37:56 +0800

    3 files modified, 7 added, 5 deleted

---
+++ App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div>
+      <h1 class="text-5xl">Marktools</h1>
+    </div>
+  );
+};
---
+++ main.tsx
@@ -0,0 +1,5 @@
+import "./main.css";
+import { render } from "solid-js/web";
+import { App } from "./App";
+
+render(() => <App />, document.getElementById("app")!);
